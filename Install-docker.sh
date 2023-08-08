c6464daf12f9deca827ee8a84da7a1e312dc9a7d
satish <venkatasatish004@gmail.com>
2023-08-08 23:11:58 +0530

    docker installation

---
+++ Install-docker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -eE -o functrace
+failure() {
+  local lineno=$1
+  local msg=$2
+  echo "Failed at $lineno: $msg"
+}
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
+USERID=$(id -u)
+R="\e[31m"
+N="\e[0m"
+
+if [$USERID -ne 0]
+then
+    echo -e "$R Please run this script with root access $N"
+    exit 1
+fi 
+
+yum update -y
+
+yum -y install docker
+
+service docker start
+
+usermod -a -G docker ec2-user
+
+chmod 666 /var/run/docker.sock
+
+docker version
