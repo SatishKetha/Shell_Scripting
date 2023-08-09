9e240da8d2a06d6ab7ea161f141a169323943a92
satish <venkatasatish004@gmail.com>
2023-08-09 13:13:34 +0530

    3rd line correction in lines, words, letters count

--- line-word-letter-count.sh
+++ line-word-letter-count.sh
@@ -5,4 +5,4 @@ echo "Lines: $(wc -l -w -c $1 | awk '{print $1 " " $2 " " $3}' | cut -d " " -f1)
 
 echo "Words: $(wc -l -w -c $1 | awk '{print $1 " " $2 " " $3}' | cut -d " " -f2)"
 
-echo "Letters: $(wc -l -w -c $1 | awk '{print $1 " " $2 " " $3}' | cut -d " " -f2)"
+echo "Letters: $(wc -l -w -c $1 | awk '{print $1 " " $2 " " $3}' | cut -d " " -f3)"
