8f263775fc650d245b30b725f58cbbed517fa72a
satish <venkatasatish004@gmail.com>
2023-08-09 13:10:55 +0530

    lines, words, letters count

---
+++ line-word-letter-count.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DIRECTORY=$1
+echo "Lines: $(wc -l -w -c $1 | awk '{print $1 " " $2 " " $3}' | cut -d " " -f1)"
+
+echo "Words: $(wc -l -w -c $1 | awk '{print $1 " " $2 " " $3}' | cut -d " " -f2)"
+
+echo "Letters: $(wc -l -w -c $1 | awk '{print $1 " " $2 " " $3}' | cut -d " " -f2)"
